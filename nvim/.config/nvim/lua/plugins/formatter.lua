518caef50d8516ffbf4081b36e4a2de26b78a233
Mantis4542 <chrismeek4542@gmail.com>
2026-03-27 17:31:35 +0000

    format on save

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -3,4 +3,27 @@ return {
     "mason-org/mason.nvim",
     opts = { ensure_installed = { "prettier" } },
   },
+
+  -- Formatting with prettier on save
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        vue = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
 }
